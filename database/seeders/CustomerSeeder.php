f8fd429418fca98482bdd456600e5667c58ec77e
Ishmam Ekaf <ishmam.bhuiyan01@northsouth.edu>
2022-08-02 11:36:13 +0600

    update

---
+++ database/seeders/CustomerSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+
+class CustomerSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        DB::table('customers')->insert([
+            'name' => Str::random(10),
+            'address' => Str::random(100),
+            'age' => rand(10,100),
+            'email' => Str::random(10).'@gmail.com'
+        ]);
+    }
+}
